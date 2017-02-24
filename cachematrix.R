78187cea4440c96139c37b95d5414fdd39221e34
OlenaKozlova <olena.kozlova.martinez@gmail.com>
2017-02-24 13:07:10 +0100

    Functions to cache the inverted matrix in the cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solve <- NULL
+    set <- function(y) {
+        x <<- y
+        solve <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(inverse) m <<- solve
+    getsolve <- function() solve
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
@@ -12,4 +22,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    solve <- x$getsolve()
+    if(!is.null(solve)) {
+        message("getting cached data")
+        return(solve)
+    }
+    ## If the matrix is not square or is its det==0 -> 
+    ## it is not invertible.
+    matr <- x$get()
+    isinversible<-class(try(solve <- solve(matr),silent=T))=="matrix"
+    if(!isinversible) {
+        message("The matrix is not inversible!")
+        return(NULL)
+    }
+    else {
+        x$setsolve(solve)
+        return(solve)
+    }
+
 }
