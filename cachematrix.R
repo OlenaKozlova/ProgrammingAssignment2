fc5fd3ddbeeb90b0c76b1a50826bc99145943096
OlenaKozlova <olena.kozlova.martinez@gmail.com>
2017-02-24 13:30:02 +0100

    Added comments in the cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions can be used to store the value of a matrix and of its inverse matrix (if such exists).
 
-## Write a short comment describing this function
+## Function with a list of getter and setter functions. Takes a matrix as an argument. 
 
 makeCacheMatrix <- function(x = matrix()) {
     solve <- NULL
@@ -18,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function returns the inverse matrix stored in cache, or computes it if the value is not cached yet.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -30,7 +29,7 @@ cacheSolve <- function(x, ...) {
     ## If the matrix is not square or is its det==0 -> 
     ## it is not invertible.
     matr <- x$get()
-    isinversible<-class(try(solve <- solve(matr),silent=T))=="matrix"
+    isinversible<-class(try(solve <- solve(matr,..),silent=T))=="matrix"
     if(!isinversible) {
         message("The matrix is not inversible!")
         return(NULL)
